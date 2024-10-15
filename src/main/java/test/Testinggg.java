3bb76ebabd94a80cf7a3cc882a9ccf10537ac883
joshuatupas <joshuatupas@gmail.com>
2024-10-15 22:20:03 +0100

    .

---
+++ src/main/java/test/Testinggg.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package test;
+public class Testinggg {
+    public static void main(String[] args) {
+        q ui = new q();
+        ui.setVisible(true);
+    }
+}
